13b21b20a239625de2f98df90d89e367f07f27dd
Christian <chrustuanr15@gmail.com>
2024-09-09 14:01:25 +0100

    create salesforce case and post comments from older issues

--- GithubWebhookHandler.py
+++ GithubWebhookHandler.py
@@ -31,8 +31,9 @@ def lambda_handler(event, context):
 
         if github_event == 'issues':
             action = data.get('action')
-            if action == 'opened':
-                create_salesforce_ticket(access_token, instance_url, issue_data, repository)
+            # Update the Salesforce ticket if labels are added, removed, or issue is edited
+            if action in ['opened', 'edited', 'labeled', 'unlabeled']:
+                update_salesforce_ticket(access_token, instance_url, issue_data, repository)
         elif github_event == 'issue_comment':
             action = data.get('action')
             if action == 'created' and comment_data:
@@ -90,21 +91,16 @@ def make_salesforce_request(url, method='GET', data=None, access_token=None, ins
     response.raise_for_status()
     return response.json()
 
-def create_salesforce_ticket(access_token, instance_url, issue_data, repository):
-    url = f"{instance_url}/services/data/v56.0/sobjects/Case"
-    data = {
-        'Subject': issue_data.get('title', 'No Title'),
-        'Description': f"Issue in repo {repository}: {issue_data.get('body', 'No Description')}",
-        'CustomField_GitHub_Issue_ID__c': issue_data.get('id', ''),
-        'CustomField_GitHub_Repo__c': repository
-    }
+def extract_github_labels(issue_data):
+    labels = issue_data.get('labels', [])
+    label_names = [label.get('name') for label in labels]
+    # Join labels into a semicolon-separated string as Salesforce expects for multi-select picklists
+    return ';'.join(label_names)
 
-    return make_salesforce_request(url, method='POST', data=data, access_token=access_token, instance_url=instance_url)
-
-def add_salesforce_comment(access_token, instance_url, issue_data, comment_data):
-    # First, query to find the Case ID using the GitHub Issue ID
+def update_salesforce_ticket(access_token, instance_url, issue_data, repository):
+    # Query to find the Case ID using the GitHub Issue number
     query_url = f"{instance_url}/services/data/v56.0/query"
-    query = f"SELECT Id FROM Case WHERE CustomField_GitHub_Issue_ID__c = '{issue_data.get('id', '')}'"
+    query = f"SELECT Id FROM Case WHERE CustomField_GitHub_Issue_ID__c = '{issue_data.get('number', '')}'"
     
     try:
         cases = make_salesforce_request(query_url, method='GET', access_token=access_token, instance_url=instance_url, data={'q': query})
@@ -113,15 +109,77 @@ def add_salesforce_comment(access_token, instance_url, issue_data, comment_data)
     
     if cases.get('totalSize', 0) > 0:
         case_id = cases['records'][0]['Id']
-        comment_url = f"{instance_url}/services/data/v56.0/sobjects/CaseComment/"
+        case_url = f"{instance_url}/services/data/v56.0/sobjects/Case/{case_id}"
         
-        # Prepare data for the new comment
+        # Extract labels from the GitHub issue
+        github_labels = extract_github_labels(issue_data)
+        
+        # Prepare data for the case update
         data = {
-            'ParentId': case_id,
-            'CommentBody': comment_data.get('body', 'No Comment')
+            'GitHub_Labels__c': github_labels  # Update the GitHub labels in Salesforce
         }
 
-        # POST request to create the new comment
-        return make_salesforce_request(comment_url, method='POST', data=data, access_token=access_token, instance_url=instance_url)
+        # POST request to update the case with the new labels
+        return make_salesforce_request(case_url, method='POST', data=data, access_token=access_token, instance_url=instance_url)
     else:
-        raise Exception(f"No case found with GitHub Issue ID: {issue_data.get('id', '')}")
+        # Create a new case if no existing one found
+        return create_salesforce_case_with_comments(access_token, instance_url, issue_data, repository)
+
+def create_salesforce_case_with_comments(access_token, instance_url, issue_data, repository):
+    url = f"{instance_url}/services/data/v56.0/sobjects/Case"
+    
+    # Construct the GitHub Issue URL
+    issue_url = issue_data.get('html_url', '')
+    
+    # Extract labels from the GitHub issue
+    github_labels = extract_github_labels(issue_data)
+
+    # Prepare data for the new case creation
+    data = {
+        'Subject': issue_data.get('title', 'No Title'),
+        'Description': f"Issue in repo {repository}: {issue_data.get('body', 'No Description')}",
+        'CustomField_GitHub_Issue_ID__c': issue_data.get('number', ''),  # Use issue number instead of id
+        'CustomField_GitHub_Repo__c': repository,
+        'GitHub_Repo_URL__c': issue_url,  # Add the issue URL to Salesforce
+        'GitHub_Labels__c': github_labels  # Set the GitHub labels in Salesforce
+    }
+
+    case = make_salesforce_request(url, method='POST', data=data, access_token=access_token, instance_url=instance_url)
+
+    # Add all existing comments from the GitHub issue to the Salesforce case
+    if 'comments' in issue_data and issue_data['comments'] > 0:
+        comments_url = issue_data.get('comments_url')
+        response = requests.get(comments_url)
+        comments = response.json()
+        for comment in comments:
+            add_salesforce_comment(access_token, instance_url, issue_data, comment)
+
+    return case['id']
+
+def add_salesforce_comment(access_token, instance_url, issue_data, comment_data):
+    # Query to find the Case ID using the GitHub Issue number
+    query_url = f"{instance_url}/services/data/v56.0/query"
+    query = f"SELECT Id FROM Case WHERE CustomField_GitHub_Issue_ID__c = '{issue_data.get('number', '')}'"
+    
+    try:
+        cases = make_salesforce_request(query_url, method='GET', access_token=access_token, instance_url=instance_url, data={'q': query})
+    except requests.exceptions.RequestException as e:
+        raise Exception(f"Error querying Salesforce: {str(e)}")
+    
+    if cases.get('totalSize', 0) == 0:
+        # If no case found, create a new case
+        case_id = create_salesforce_case_with_comments(access_token, instance_url, issue_data, issue_data.get('repository', ''))
+    else:
+        case_id = cases['records'][0]['Id']
+    
+    # Add the comment to the case (newly created or existing)
+    comment_url = f"{instance_url}/services/data/v56.0/sobjects/CaseComment/"
+    
+    # Prepare data for the new comment
+    data = {
+        'ParentId': case_id,
+        'CommentBody': comment_data.get('body', 'No Comment')
+    }
+
+    # POST request to create the new comment
+    return make_salesforce_request(comment_url, method='POST', data=data, access_token=access_token, instance_url=instance_url)
